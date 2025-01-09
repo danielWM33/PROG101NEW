74c4750c7a4105d195d27fe82901f2816d8708cf
danielWM33 <dwong-moon@ucsb.edu>
2025-01-09 08:12:55 -0800

    PROG101NEW
    DONE!!!!!!
    1/9/25
    PROG101

---
+++ PROG101NEW.R
@@ -0,0 +1,21 @@
+#Calling function is name followed by parenthesis
+sqrt(9)
+abs (-3)
+
+sqrt(9249)
+sqrt(infinity)
+#Autocompleting is coolio
+
+#str() = structure
+str(3)
+str("fjnjasbgf")
+#Green = synthax highlighting\
+\
+
+#Func can also round
+pi
+round(pi, 21)
+round(pi, 2)
+
+?sqrt
+?floor
